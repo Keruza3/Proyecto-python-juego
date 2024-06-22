a85acca2e8875a69dff9a75f243a0fd9d55500ba
Mati-Vargados <159140916+Mati-Vargados@users.noreply.github.com>
2024-06-21 23:25:02 -0300

    main.py

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ if ventana != None:
     
     pantalla_seleccion = pantalla_seleccion_partida(ventana)
 
-    ventana = pantalla_seleccion[0]
-    nombre_partida = pantalla_seleccion[1]
-    
-
-    if (pantalla_seleccion != None and
-        (nombre_partida.capitalize() == "Partida_1" or
-        nombre_partida.capitalize() == "Partida_2" or 
-        nombre_partida.capitalize() == "Partida_3" or 
-        nombre_partida.capitalize() == "Partida_4")):
-
-        pantalla_ingresar_nombre(ventana)
-
     if pantalla_seleccion != None:
 
-        pass
+        ventana = pantalla_seleccion[0]
+        nombre_partida = pantalla_seleccion[1]
+
+        if (nombre_partida == "Partida_1" or
+            nombre_partida == "Partida_2" or 
+            nombre_partida == "Partida_3" or 
+            nombre_partida == "Partida_4"):
+
+            pantalla_ingresar_nombre(ventana)
+
         # pantalla_juego()
+
